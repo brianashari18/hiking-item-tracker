e8020e9806951548ce301f730d78364ff760a29e
Brian Anashari <brianashari18@gmail.com>
2026-04-14 15:44:50 +0700

    Initial Commit

---
+++ HikingItemTracker/HikingItemTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingItemTrackerApp.swift
+//  HikingItemTracker
+//
+//  Created by Brian Anashari on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HikingItemTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
